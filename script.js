ac8d470067f740ad61bce776bf5de5b4f7d0ad9a
Mary Do <marydo318@gmail.com>
2019-07-10 17:39:31 -0700

    added fetch and some layout styles

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-const 
+const form = document.getElementById('form');
+
+const getBeer = (e) => {
+  const foodQuery = document.getElementById('food-input').value;
+
+  fetch(`https://api.punkapi.com/v2/beers?food=${foodQuery}`)
+  .then(
+    res => {
+      return res.json()
+    }
+  )
+  .then(
+    data => {
+      console.log(data);
+      let output = '';
+      let foodList = '';
+      data.map((beer) => {
+        beer.food_pairing.map(food => {
+          foodList += `<li>${food}</li>`;
+        });
+        output += `
+          <ul>
+            <li><img src="${beer.image_url}"></li>
+            <li>${beer.name}</li>
+            <li>ABV: ${beer.abv}</li>
+            <li>This beer is great with:
+              <ul>${foodList}</ul>
+            </li>
+          </ul>
+        `;
+      });
+      document.getElementById('output').innerHTML = output;
+    }
+  )
+  e.preventDefault();
+}
+
+form.addEventListener('submit', getBeer);
